73b1776c2c5373e08bdc613880cb43e55bfde870
avkuznetsov54 <avkuznetsov54@gmail.com>
2020-04-12 16:51:18 +0700

    053 Обработка ошибок в Fetch API

--- src/index.js
+++ src/index.js
@@ -6,22 +6,21 @@
 //   document.getElementById('root')
 // );
 
-// fetch('http://gateway.marvel.com/v1/public/characters?apikey=2b160ff00ade6892a67efc3ae016906c&ts=1&hash=648af0b16539b34c875ebd06950e0b4a&offset=2')
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((body) => {
-//     console.log(body);
-//   });
 
 const  getResource = async (url) => {
   const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` + `, received ${res.status}`);
+  }
   const body = await res.json();
   return body;
 };
 
-getResource('http://gateway.marvel.com/v1/public/characters/1010354?apikey=2b160ff00ade6892a67efc3ae016906c&ts=1&hash=648af0b16539b34c875ebd06950e0b4a')
+getResource('http://gateway.marvel.com/v1/public/characters/10103542?apikey=2b160ff00ade6892a67efc3ae016906c&ts=1&hash=648af0b16539b34c875ebd06950e0b4a')
   .then((body) => {
     console.log(body);
+  })
+  .catch((err) => {
+    console.error(err);
   });
 
